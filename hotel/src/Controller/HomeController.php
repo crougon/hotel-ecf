2880416dd35d1b26bf4a3fe6f364f0d8220edfba
crougon <83468135+crougon@users.noreply.github.com>
2022-04-05 21:25:37 +0200

    add show and modified home

--- hotel/src/Controller/HomeController.php
+++ hotel/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RoomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,18 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(RoomRepository $ro )
     {
+
+        $rooms = $ro->findAll();
+
+        $random = array_rand($rooms, 2);
+
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'rooms1' => $rooms[$random[0]],
+            'rooms2' => $rooms[$random[1]]
         ]);
     }
 }
