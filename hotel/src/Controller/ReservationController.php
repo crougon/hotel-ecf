3228cdecc13d72f4c698f07b58b166e02845eda6
crougon <83468135+crougon@users.noreply.github.com>
2022-04-06 20:24:27 +0200

    edit reservation

--- hotel/src/Controller/ReservationController.php
+++ hotel/src/Controller/ReservationController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
 use App\Entity\Room;
+use App\Repository\ReservationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,23 +18,80 @@ class ReservationController extends AbstractController
     /**
      * @Route("/reservation", name="app_reservation")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('reservation/index.html.twig', [
-            'controller_name' => 'ReservationController',
-        ]);
+        //return $this->render('reservation/index.html.twig', [
+        //    'controller_name' => 'ReservationController',
+        //]);
     }
 
     /**
      * @Route("/reserve/{id}", name="reserve")
      */
 
-    public function reservation(Room $room){
+    public function reserve(Room $room, ReservationRepository $res, Request $request){
+
+        $resform = $this->createFormBuilder()
+        ->add('email', EmailType::class, [
+            'label' => 'Email',
+            'attr' => ['placeholder' => 'email']
+        ])
+        ->add('datein', DateType::class, [
+            'label' => 'Date d\'entrée',
+            'placeholder' => [
+                'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour'
+            ]
+        ])
+        ->add('dateout', DateType::class, [
+            'label' => 'Date de sortie',
+            'placeholder' => [
+                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour'
+            ]
+        ])
+        ->add('Réserver', SubmitType::class)
+        ->getForm();   
+
+        $resform->handleRequest($request);
+
+        if ($resform->isSubmitted()){
+            $input = $resform->getData();
+
+            $reservation = new Reservation();
+
+            $reservation->setEmail($input['email']);
+
+            $reservation->setDatein($input['datein']);
+
+            $reservation->setDateout($input['dateout']);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($reservation);
+            $em->flush();
 
 
-       // $this->addFlash('reserver', $reserve->getName(). 'a été réservé')
 
-        return $this->redirect($this->generateUrl(''));
+            $this->addFlash('reserver', 'La chambre a été réservé');
+    
+            return $this->redirect($this->generateUrl('home'));
+        
+        }
 
+        return $this->render('room/show/{id}.html.twig', [
+            'resform' => $resform->createView()
+        ]);
+        
+        
+        
+        
     }
+    
+    
 }
+
+//--------------------------
+
+//$reservation = new Reservation();
+//$reservation->setDatein($res->getDatein());
+//$reservation->setDateout($res->getDateOut());
+//$reservation->setPrice($room->getPrice());
+//$reservation->setStatus("Réservé");
--- hotel/src/Entity/Reservation.php
+++ hotel/src/Entity/Reservation.php
@@ -17,8 +17,93 @@ class Reservation
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datein;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateout;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getDatein(): ?\DateTimeInterface
+    {
+        return $this->datein;
+    }
+
+    public function setDatein(\DateTimeInterface $datein): self
+    {
+        $this->datein = $datein;
+
+        return $this;
+    }
+
+    public function getDateout(): ?\DateTimeInterface
+    {
+        return $this->dateout;
+    }
+
+    public function setDateout(\DateTimeInterface $dateout): self
+    {
+        $this->dateout = $dateout;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
     }
 }
