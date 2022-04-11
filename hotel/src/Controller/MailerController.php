45f9ecb2ccd8a84ed812a8701af56d5cc6f60994
crougon <83468135+crougon@users.noreply.github.com>
2022-04-11 20:39:11 +0200

    finished mailer

--- hotel/src/Controller/MailerController.php
+++ hotel/src/Controller/MailerController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
@@ -14,19 +18,57 @@ class MailerController extends AbstractController
     /**
      * @Route("/mail", name="mail")
      */
-    public function sendEmail(MailerInterface $mailer): Response
-    {
+    public function sendEmail(MailerInterface $mailer, Request $request): Response
+    {   
+        $emailForm = $this->createFormBuilder()
+            ->add('user', TextType::class, [
+            'label' => 'email',
+            'attr' => ['placeholder' => 'Email']
+        ])  
+            ->add('message', TextareaType::class, [
+                'attr' => array('rows' => '5')
+            ])
+            ->add('send', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-outline-danger  float-right'
+                ]
+            ])
 
+            ->getForm();
+
+            $emailForm->handleRequest($request);
+
+            if ($emailForm->isSubmitted()){
+
+                $input = $emailForm->getData();
+                $from = ($input['user']);
+                $text = ($input['message']);
+
+            
+
+        
         $email = (new TemplatedEmail())
-            ->from('hotel@hotel.wip')
-            ->to('client@hotel.com') //
+
+            ->from($from) //
+            ->to('hotel@hotel.wip')
             ->subject('Réservation ')
-            ->text('')
+            
 
             ->htmlTemplate('mailer/mail.html.twig')
+
+            ->context([ 
+                'text' => $text,
+                'from' => $from
+             ]);
         ;
         $mailer->send($email);
-
-        return new Response('Email envoyé');
+        $this->addFlash('message', 'Votre message a été transmis');
+        return $this->redirect($this->generateUrl('mail'));
     }
+        
+        return $this->render('mailer/index.html.twig', [
+        'emailForm' => $emailForm->createView()
+    ]);
+    }
+
 }
